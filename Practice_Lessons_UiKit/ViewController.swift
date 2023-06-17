4fe61a20e6a509ce40ce6903d7ebb57b90691e82
Anton <xxahtoxaxx2015@gmail.com>
2023-06-17 09:54:27 +0400

    Initial Commit

---
+++ Practice_Lessons_UiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice_Lessons_UiKit
+//
+//  Created by Антон Исаков on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
